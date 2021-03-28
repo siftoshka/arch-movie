28f8c44be68e0d03b20fcced8a14b354e1216f29
amorphist <azad.mamiyev.98@gmail.com>
2021-03-28 19:59:53 +0400

    Added first navigation

---
+++ mvp/src/main/java/cz/mamiyaza/mvp/App.kt
@@ -0,0 +1,15 @@
+package cz.mamiyaza.mvp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Application class of the MVP.
+ */
+@HiltAndroidApp
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ mvp/src/main/java/cz/mamiyaza/mvp/MainFragment.kt
@@ -0,0 +1,12 @@
+package cz.mamiyaza.mvp
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+/**
+ * TODO add class description
+ */
+@AndroidEntryPoint
+class MainFragment : Fragment(R.layout.include_main_screen) {
+
+}
